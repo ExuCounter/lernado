349179cbb905eee8cc4cd2f1c409b9512965f656
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2025-04-25 16:46:54 +0300

    feat: update configs

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -13,6 +13,10 @@ config :backend, Backend.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
+config :backend, :aws,
+  dispatcher: Backend.AWS.Dispatcher.Mock,
+  courses_bucket: "dummy_bucket"
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :backend, BackendWeb.Endpoint,
