1d746fd178dd41ea797c7e1995b656bebee7630f
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2025-04-28 08:35:29 +0300

    fix: duplicated env variables

--- backend/test/test_helper.exs
+++ backend/test/test_helper.exs
@@ -1,10 +1,5 @@
+Mox.defmock(Backend.AWS.DispatcherMock, for: Backend.AWS.Dispatcher)
+
 ExUnit.start()
 Faker.start()
 Ecto.Adapters.SQL.Sandbox.mode(Backend.Repo, :manual)
-
-Mox.defmock(Backend.AWS.DispatcherMock, for: Backend.AWS.Dispatcher)
-
-Application.put_env(:backend, :aws,
-  dispatcher: Backend.AWS.DispatcherMock,
-  courses_bucket: "courses_dummy_bucket"
-)
